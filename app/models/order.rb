c1784c9e0c487e5c711ca1a550ae8a0255542f87
David Hale <david.hale7@gmail.com>
2017-12-08 11:04:21 -0800

    trying to get ajax working

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   before_save :calculate_total
 
   def calculate_total
+    # binding.pry
     self.total_price = order_items.collect { |item| item.product.price * item.quantity }.sum
   end
 
