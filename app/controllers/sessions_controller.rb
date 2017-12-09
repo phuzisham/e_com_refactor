8a77b7504ee50a7cd3a8e67713eb7f1e01f15945
David Hale <david.hale7@gmail.com>
2017-12-08 16:56:21 -0800

    flash notice on sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "You've successfully signed in!"
       redirect_to '/'
     else
       redirect_to '/sign_in'
