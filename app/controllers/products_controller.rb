96624f383e844d918884c52cca7e088d2323e30c
David Hale <david.hale7@gmail.com>
2017-12-11 18:03:26 -0800

    add edit and delete for admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,26 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)
